d030455e6774f2c41afd066c97f4cc61b04d9268
Bair <liberty2217@yandex.ru>
2020-10-25 13:40:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,18 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    
+        let result = '';
+        for (let i = 0; i < expr.length; i += 10) {
+            const subStr = expr.substring(i, i + 10);
+            if (subStr === '**********') {
+                result += ' ';
+                continue;
+            }
+            result += MORSE_TABLE[subStr.replace(/10/g,'.').replace(/11/g,'-').replace(/0/g, '')];
+        }
+        return result;
+      }
 
 module.exports = {
     decode
